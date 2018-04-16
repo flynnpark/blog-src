04be812f3f17977b845826605464547affd94732
Flynn <wphestiraid@gmail.com>
2018-04-16 14:45:44 +0900

    config 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: 'Flynndev',
+    author: 'Flynn <wphestiraid@gmail.com>',
+    keywords: ['dev', 'blog', 'web'],
   },
   plugins: [
     'gatsby-plugin-react-helmet',
