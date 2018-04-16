c025675ff88da37d4d933aa7b886836eb5192280
Flynn <wphestiraid@gmail.com>
2018-04-16 17:45:57 +0900

    ProfileCard 디자인 및 정보 수정

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Icon } from 'semantic-ui-react';
+
+import globalConfig from '../../global-config';
 
 const ProfileCard = () => {
+  const { nickname, realname, job, github } = globalConfig;
+
   return (
     <Card>
       <Image />
       <Card.Content>
-        <Card.Header>Flynn (Inho Park)</Card.Header>
-        <Card.Meta>Common Developer</Card.Meta>
+        <Card.Header>
+          {nickname} ({realname})
+        </Card.Header>
+        <Card.Meta>{job}</Card.Meta>
+      </Card.Content>
+      <Card.Content extra>
+        <a href={`https://github.com/${github}`} target="_blank">
+          <Icon name="github" />
+          {github}
+        </a>
       </Card.Content>
     </Card>
   );
