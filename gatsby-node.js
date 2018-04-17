2d330862f9abdd40ad857dcdcc2a0881eca6a2c4
Flynn <wphestiraid@gmail.com>
2018-04-17 15:36:08 +0900

    태그 지원 추가
    - /tags 페이지 생성
    - 태그별 글 목록 페이지 생성

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,6 +6,7 @@
 
 // You can delete this file if you're not using it
 const path = require('path');
+const _ = require('lodash');
 const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
@@ -28,31 +29,57 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators;
-  const blogPostTemplate = path.resolve(`./src/templates/PostTemplate.js`);
-  return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        posts: allMarkdownRemark {
-          edges {
-            node {
-              fields {
-                slug
-              }
+  const blogPostTemplate = path.resolve('./src/templates/post.js');
+  const tagTemplate = path.resolve('./src/templates/tag.js');
+
+  return graphql(`
+    {
+      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              tags
             }
           }
         }
       }
-    `).then(result => {
-      result.data.posts.edges.forEach(({ node }) => {
-        createPage({
-          path: node.fields.slug,
-          component: blogPostTemplate,
-          context: {
-            slug: node.fields.slug,
-          }, // additional data can be passed via context
-        });
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+
+    const posts = result.data.allMarkdownRemark.edges;
+
+    posts.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: blogPostTemplate,
+        context: {
+          slug: node.fields.slug,
+        }, // additional data can be passed via context
       });
-      resolve();
+    });
+
+    let tags = [];
+    _.each(posts, edge => {
+      if (_.get(edge, 'node.frontmatter.tags')) {
+        tags = tags.concat(edge.node.frontmatter.tags);
+      }
+    });
+    tags = _.uniq(tags);
+
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag)}/`,
+        component: tagTemplate,
+        context: {
+          tag,
+        },
+      });
     });
   });
 };
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,7 +3,6 @@ import Link from 'gatsby-link';
 import { Menu, Container } from 'semantic-ui-react';
 
 const NavigationBar = ({ siteTitle }) => {
-  console.log(siteTitle);
   return (
     <Menu borderless fixed="top" inverted>
       <Container>
