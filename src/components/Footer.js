a7fb77bd4e51248dfdc548ea68fb61c18d6db876
Flynn <wphestiraid@gmail.com>
2018-05-09 16:11:46 +0900

    Footer에 링크 추가
    - Footer에 링크 추가
    - 구분 문자 변경('/' → '·')
    - Semantic UI 기본 폰트 크기 14로 재조정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,22 @@ const Footer = () => {
   return (
     <Segment vertical style={{ marginTop: '1.5em' }}>
       <Container textAlign="center">
-        © 2018 Flynn (Inho Park) / Built with Gatsby and React / Hosted by
-        Netlify
+        © 2018 Flynn (Inho Park) · Built with{' '}
+        <a href="https://www.gatsbyjs.org/" target="_blank">
+          Gatsby
+        </a>{' '}
+        &{' '}
+        <a href="https://reactjs.org/" target="_blank">
+          React
+        </a>{' '}
+        · Hosted by{' '}
+        <a href="https://www.netlify.com/" target="_blank">
+          Netlify
+        </a>{' '}
+        · Source code on{' '}
+        <a href="https://github.com/wphestiraid/blog-src" target="_blank">
+          Github
+        </a>
       </Container>
     </Segment>
   );
