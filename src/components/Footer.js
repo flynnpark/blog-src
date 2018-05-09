4fe60392425232f3c8232bd511ea54942839310e
Flynn <wphestiraid@gmail.com>
2018-05-09 17:13:03 +0900

    Footer 내용 List로 변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import { Container, Segment } from 'semantic-ui-react';
+import { Container, Segment, List } from 'semantic-ui-react';
 
 const Footer = () => {
   return (
     <Segment vertical style={{ marginTop: '1.5em' }}>
       <Container textAlign="center">
-        © 2018 Flynn (Inho Park) · Built with{' '}
-        <a href="https://www.gatsbyjs.org/" target="_blank">
-          Gatsby
-        </a>{' '}
-        &{' '}
-        <a href="https://reactjs.org/" target="_blank">
-          React
-        </a>{' '}
-        · Hosted by{' '}
-        <a href="https://www.netlify.com/" target="_blank">
-          Netlify
-        </a>{' '}
-        · Source code on{' '}
-        <a href="https://github.com/wphestiraid/blog-src" target="_blank">
-          Github
-        </a>
+        <List horizontal list>
+          <List.Item>© 2018 Flynn (Inho Park)</List.Item>
+          <List.Item>
+            Built with{' '}
+            <strong>
+              <a href="https://www.gatsbyjs.org/" target="_blank">
+                Gatsby
+              </a>
+            </strong>{' '}
+            &{' '}
+            <strong>
+              <a href="https://reactjs.org/" target="_blank">
+                React
+              </a>
+            </strong>
+          </List.Item>
+          <List.Item>
+            Hosted by{' '}
+            <strong>
+              <a href="https://www.netlify.com/" target="_blank">
+                Netlify
+              </a>
+            </strong>
+          </List.Item>
+          <List.Item>
+            Source code on{' '}
+            <strong>
+              <a href="https://github.com/wphestiraid/blog-src" target="_blank">
+                Github
+              </a>
+            </strong>
+          </List.Item>
+        </List>
       </Container>
     </Segment>
   );
