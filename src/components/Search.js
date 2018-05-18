56805bb83691d14db59dad9a9483066a118ad789
Flynn <wphestiraid@gmail.com>
2018-05-18 16:54:26 +0900

    검색 결과 디자인 수정
    - 검색 결과를 Semantic UI CSS를 사용하여 디자인
    - 검색 결과 팬만 수정

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Popup } from 'semantic-ui-react';
+import Link from 'gatsby-link';
+import { Segment, Item } from 'semantic-ui-react';
 import {
   InstantSearch,
   SearchBox,
@@ -17,27 +18,32 @@ const Search = props => {
       apiKey={algolia.searchOnlyApiKey}
       indexName={algolia.indexName}
     >
-      <Popup
-        trigger={<SearchBox translations={{ placeholder: 'Search...' }} />}
-        on="click"
-        wide
-        hideOnScroll
-      >
-        <Stats />
-        <Pagination />
-      </Popup>
+      <div className="ui input search right aligned">
+        <SearchBox translations={{ placeholder: 'Search...' }} />
+        <div className="results transition visible" style={{ width: '36em' }}>
+          <Hits hitComponent={Hit} />
+          <Stats />
+          <Pagination />
+        </div>
+      </div>
     </InstantSearch>
   );
 };
 
 const Hit = props => {
+  console.log(props);
   const { classes, hit } = props;
-
   return (
-    <Link to={hit.fields.slug} className={classes.link}>
-      {hit.frontmatter.title}
-      {hit.frontmatter.subTitle && <span>{hit.frontmatter.subTitle}</span>}
-    </Link>
+    <Item className="ui input">
+      <Item.Image
+        size="tiny"
+        src={hit.frontmatter.cover.childImageSharp.resize.coverImage}
+      />
+      <Item.Content className="ui input">
+        <Item.Header>{hit.frontmatter.title}</Item.Header>
+        <Item.Content>{hit.excerpt}</Item.Content>
+      </Item.Content>
+    </Item>
   );
 };
 
